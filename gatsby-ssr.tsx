42fcdbfe1b241fe1bf2c314221dc5ea37faa508d
JHernandez <james@jahtx.com>
2025-04-26 20:49:40 -0500

    working on mckbook air m4

--- gatsby-ssr.tsx
+++
@@ -1,7 +0,0 @@
-// Adds a class name to the body element
-exports.onRenderBody = ({ setBodyAttributes, setHtmlAttributes }) => {
-  setHtmlAttributes({ lang: 'en' });
-  // setBodyAttributes({
-  //   className: 'dark',
-  // });
-};
